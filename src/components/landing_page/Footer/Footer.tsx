92b53fa524c577161598218ac6e51c0e696292c3
leopan <tu416164@gmail.com>
2024-05-16 14:53:43 +0700

    fix

--- src/components/landing_page/Footer/Footer.tsx
+++ src/components/landing_page/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import FooterLogo from '~/assets/svg/Tdocman.svg'
 import { FaFacebook, FaInstagram, FaLinkedin, FaLocationArrow, FaMobileAlt } from 'react-icons/fa'
 // import NatureVid from '~/assets/video/c.mp4'
+import ImgFooter from '~/assets/places/business-idea-3683781_1920.jpg'
 import { Link } from 'react-router-dom'
 
 const FooterLinks = [
@@ -29,8 +30,13 @@ const Footer = () => {
         {/* <video autoPlay loop muted className='absolute right-0 top-0 h-full overflow-hidden w-full object-cover z-[-1]'>
           <source src={NatureVid} type='video/mp4' />
         </video> */}
+        <img
+          src={ImgFooter}
+          className='absolute right-0 top-0 h-full overflow-hidden w-full object-cover z-[-1]'
+          alt=''
+        />
         <div className='container'>
-          <div className='grid md:grid-cols-3 py-5 bg-teal-500  rounded-t-xl text-white'>
+          <div className='grid md:grid-cols-3 py-5 bg-gray-200 rounded-t-xl'>
             <div className='py-8 px-4'>
               <h1 className='flex items-center gap-3 text-xl sm:text-3xl font-bold text-justify sm:text-left'>
                 <img src={FooterLogo} alt='' className='max-h-[60px] color-white' />
@@ -73,7 +79,7 @@ const Footer = () => {
                   </h1>
                   <ul className="flex flex-col gap-3">
                     {FooterLinks.map((link) => (
-                      <li className='cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-200 dark:text-black-500'>
+                      <li className='cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 dark:text-black-500'>
                         <Link to={link.link} onClick={() => window.scrollTo(0, 0)}>
                           <span>&#11162;</span>
                           <span>{link.title}</span>
@@ -90,7 +96,7 @@ const Footer = () => {
                   </h1>
                   <ul className="flex flex-col gap-3">
                     {FooterLinks.map((link) => (
-                      <li className='cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-200 dark:text-black-500'>
+                      <li className='cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 dark:text-black-500'>
                         <Link to={link.link} onClick={() => window.scrollTo(0, 0)}>
                           <span>&#11162;</span>
                           <span>{link.title}</span>
@@ -107,7 +113,7 @@ const Footer = () => {
                   </h1>
                   <ul className="flex flex-col gap-3">
                     {FooterLinks.map((link) => (
-                      <li className='cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-200 dark:text-black-500'>
+                      <li className='cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 dark:text-black-500'>
                         <Link to={link.link} onClick={() => window.scrollTo(0, 0)}>
                           <span>&#11162;</span>
                           <span>{link.title}</span>
@@ -121,7 +127,7 @@ const Footer = () => {
           </div>
           <div>
             <div className="text-center py-5 border-t-2 border-gray-300/50 bg-primary text-white">
-              @copyright 2024 All rights reserved || Made with ❤️ by Tudeptrai
+              @copyright 2024 All rights reserved || Made with ❤️ by G-63
             </div>
           </div>
         </div>
