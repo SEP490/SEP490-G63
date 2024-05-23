61d9f6e2d64f7b5778dab87187d91095d933459a
leopan <tu416164@gmail.com>
2024-05-23 23:36:36 +0700

    refactor(SendMail.tsx): update layout

--- src/components/Admin/SendMail/ComboboxMail.tsx
+++ src/components/Admin/SendMail/ComboboxMail.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { MultiSelect } from 'react-multi-select-component'
+import { MdClear } from 'react-icons/md'
 
 type Option = {
   label: string
@@ -7,6 +8,9 @@ type Option = {
 }
 
 const options: Option[] = [
+  { label: 'Junggie@gmail.com', value: 'grapes' },
+  { label: 'Yujinnie@gmail.com', value: 'mango' },
+  { label: 'Minjeong@gmail.com', value: 'strawberry' },
   { label: 'Junggie@gmail.com', value: 'grapes' },
   { label: 'Yujinnie@gmail.com', value: 'mango' },
   { label: 'Minjeong@gmail.com', value: 'strawberry' }
@@ -17,12 +21,27 @@ const handleNewField = (value: string): Option => ({
   value: value.toUpperCase().replace(/\s+/g, '_')
 })
 
-const customValueRenderer = (selected: any, _options: any) => {
-  return selected.length ? selected.map(({ label }: any) => label) : 'Tìm kiếm'
-}
-
 const ComboboxMail = () => {
   const [selected, setSelected] = useState<Option[]>([])
+
+  const handleRemove = (value: string, event: React.MouseEvent) => {
+    event.stopPropagation()
+    setSelected(selected.filter((option) => option.value !== value))
+  }
+
+  const customValueRenderer = (selected: Option[], _options: Option[]) => {
+    return selected.length
+      ? selected.map(({ label, value }: Option) => (
+          <div key={value} className='inline-block bg-slate-200 mx-1 px-4 py-1 rounded relative'>
+            {label}
+            <MdClear
+              className='absolute top-1 right-0 cursor-pointer'
+              onClick={(event) => handleRemove(value, event)}
+            />
+          </div>
+        ))
+      : 'Tìm kiếm'
+  }
 
   return (
     <div className='w-full'>
--- src/pages/Admin/SendMail.tsx
+++ src/pages/Admin/SendMail.tsx
@@ -6,25 +6,25 @@ import uploadIcon from '~/assets/images/uploadpdf.png'
 const SendMail = () => {
   return (
     <>
-      <div className='place-content-center bg-slate-200 p-12 rounded m-2 shadow-xl shadow-neutral-400'>
-        <div className='bg-slate-300 rounded p-2 text-lg'>
+      <div className='place-content-center m-8 rounded p-6 shadow-xl shadow-neutral-400 w-2/3 mx-auto'>
+        <div className='font-bold rounded text-3xl mb-8'>
           <span>Thư mới</span>
         </div>
-        <div className='py-2 border-b-2 border-slate-400 flex items-center'>
-          <span className='w-16'>Đến</span>
+        <div className='py-2 border-b-2 border-slate-200 flex items-center'>
+          <span className='w-20 font-bold'>Đến</span>
           {/* <input type='text' className='w-full border-none rounded'></input> */}
           <ComboboxMail />
         </div>
-        <div className='py-2 border-b-2 border-slate-400 flex items-center'>
-          <span className='w-16'>CC</span>
+        <div className='py-2 border-b-2 border-slate-200 flex items-center'>
+          <span className='w-20 font-bold'>CC</span>
           <ComboboxMail />
           {/* <input type='text' className='w-full border-none rounded'></input> */}
         </div>
-        <div className='py-2 border-b-2 border-slate-400 flex items-center'>
-          <span className='w-16'>Tiêu đề</span>
-          <input type='text' className='w-full rounded'></input>
+        <div className='py-2 border-b-2 border-slate-200 flex items-center'>
+          <span className='w-20 font-bold'>Tiêu đề</span>
+          <input type='text' className='border-none w-full rounded h-8' placeholder=''></input>
         </div>
-        <h2 className='mt-4'>Nội dung</h2>
+        <h2 className='mt-8 font-bold mb-4'>Nội dung</h2>
         <CKEditor
           editor={ClassicEditor}
           data='<p>Go Youn-jung (Tiếng Hàn: 고윤정; sinh ngày 22 tháng 4 năm 1996) là nữ diễn viên và người mẫu người Hàn Quốc.[2] Cô xuất hiện lần đầu trong bộ phim truyền hình Chàng trai ngoại cảm (2019) và đã được công nhận với vai phụ trong phim Netflix Sweet Home: Thế giới ma quái (2020). Cô còn được biết đến qua các bộ phim truyền hình Trường luật (2021), Hoàn hồn (2022–2023) và phim gốc Disney+ Moving (2023) cũng như phim Săn lùng (2022).</p>'
@@ -42,15 +42,12 @@ const SendMail = () => {
             console.log('Focus.', editor)
           }}
         />
-        <div className="mt-4 text-center">
-          <input type="file" id="file-upload" className="hidden" />
-          <label htmlFor="file-upload" className="cursor-pointer inline-flex items-center justify-center rounded-full">
-            <img src={uploadIcon} alt="Upload" className="w-30 h-20" />
+        <div className='mt-4 text-center'>
+          <input type='file' id='file-upload' className='hidden' />
+          <label htmlFor='file-upload' className='cursor-pointer inline-flex items-center justify-center rounded-full'>
+            <img src={uploadIcon} alt='Upload' className='w-30 h-20' />
           </label>
         </div>
-        {/* <button className='p-2 rounded-full bg-teal-500 text-white mt-4 text-center flex justify-center mx-auto'>
-          Đính kèm tệp
-        </button> */}
       </div>
       <button className='py-2 px-6 mb-4 rounded bg-sky-800 text-white mt-4 text-center flex justify-center mx-auto hover:bg-sky-500 active:bg-sky-800 focus:outline-none focus:ring focus:ring-sky-300'>
         Gửi
