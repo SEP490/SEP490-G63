f3eee4253d55a081255a9ecf4d2fb0c4e54684a6
leopan <tu416164@gmail.com>
2024-06-09 14:02:02 +0700

    feat: adding Home page by role

---
+++ src/pages/AdminOfficer/HomeAdminOfficer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeAdminOfficer: React.FC = () => {
+  return (
+    <div>
+      <h1>HomeAdminOfficer</h1>
+    </div>
+  )
+}
+
+export default HomeAdminOfficer
---
+++ src/pages/Sale/HomeSale.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeSale: React.FC = () => {
+  return (
+    <div>
+      <h1>HomeSale</h1>
+    </div>
+  )
+}
+
+export default HomeSale
