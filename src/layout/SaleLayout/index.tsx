98b597262fe7d8bb4448ff95b65385d8db07c612
leopan <tu416164@gmail.com>
2024-06-09 13:59:39 +0700

    feat: adding sale layout

---
+++ src/layout/SaleLayout/index.tsx
@@ -0,0 +1,30 @@
+import { Navigate, useLocation } from 'react-router-dom'
+import { useAuth } from '~/provider/authProvider'
+import NavBar from './NavBar'
+import useViewport from '~/hooks/useViewport'
+import { SALE } from '~/common/const/role'
+
+function SaleLayout({ children }: any) {
+  const location = useLocation()
+  const { token, user } = useAuth()
+  const { width } = useViewport()
+  const isMobile = width <= 1024
+  if (!token && user?.role !== SALE) {
+    return <Navigate to='/404notfound' state={{ from: location }} replace></Navigate>
+  }
+  return (
+    <div className='h-[100vh] overflow-hidden bg-[#e8eaed]'>
+      <div className='fixed w-full z-30'>
+        <NavBar />
+      </div>
+
+      <div
+        className={`${isMobile ? 'mt-[50px] h-[calc(100vh-50px)]' : 'mt-[100px] h-[calc(100vh-100px)]'} overflow-auto`}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default SaleLayout
