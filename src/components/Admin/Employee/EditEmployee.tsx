a88bb8209ed810d59b9e219c003332fda6fc6ff8
ThangNH <nhoctrumgenz2002@gmail.com>
2024-07-09 14:55:05 +0700

    fix udpdate information employee

--- src/components/Admin/Employee/EditEmployee.tsx
+++ src/components/Admin/Employee/EditEmployee.tsx
@@ -1,10 +1,11 @@
 import { ChangeEventHandler, EventHandler, useMemo, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { useMutation } from 'react-query'
+import LoadingIcon from '~/assets/LoadingIcon'
 import permissionsList from '~/common/const/permissions'
-import { REGEX_EMAIL } from '~/common/const/regexForm'
+import { currentDate } from '~/common/utils/formatDate'
 import useToast from '~/hooks/useToast'
 import { DataEmployee } from '~/pages/Admin/Employee'
-import { createEmployee } from '~/services/employee.service'
 import { updateProfile } from '~/services/user.service'
 type FromType = {
   password: string
@@ -32,19 +33,22 @@ const EditEmployee = ({ data, closeModal, refetch }: IProp) => {
     formState: { errors }
   } = useForm<FromType>({ defaultValues: { ...data, permissions: data?.permissions?.slice(1, -1) } })
   const { successNotification, errorNotification } = useToast()
-  const onSubmit: SubmitHandler<FromType> = async (dataForm) => {
-    try {
-      const response = await updateProfile(data?.id as string, { ...dataForm, permissions: [dataForm.permissions] })
+  const editEmployee = useMutation(updateProfile, {
+    onSuccess: (response) => {
       if (response.code == '00' && response.object) {
         successNotification('Chỉnh sửa thông tin người dùng thành công')
         closeModal()
         refetch()
       } else errorNotification('Chỉnh sửa thông tin người dùng thất bại')
-    } catch (error) {
-      console.log(error)
+    },
+    onError: () => {
       errorNotification('Lỗi hệ thống')
     }
+  })
+  const onSubmit: SubmitHandler<FromType> = async (dataForm) => {
+    editEmployee.mutate({ id: data?.id as string, formData: { ...dataForm, permissions: [dataForm.permissions] } })
   }
+  console.log(errors)
 
   return (
     <form
@@ -67,20 +71,30 @@ const EditEmployee = ({ data, closeModal, refetch }: IProp) => {
         </div>
       </div>
       <div className='w-[100%] sm:w-[48%] md:w-[29%] mt-5 relative'>
-        <label className='font-bold '>Phòng ban</label>
+        <label className='font-bold '>
+          Phòng ban<sup className='text-red-500'>*</sup>
+        </label>
         <input
-          className={`block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
+          className={`${errors.department ? 'ring-red-600' : ''} block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
           {...register('department')}
-          placeholder='Nhập phòng ban nhân viên'
+          placeholder='Phòng ban'
         />
+        <div className={`text-red-500 absolute text-[12px] ${errors.department ? 'visible' : 'invisible'}`}>
+          {errors.department?.message}
+        </div>
       </div>
       <div className='w-[100%] sm:w-[48%] md:w-[29%] mt-5 relative'>
-        <label className='font-bold '>Vị trí</label>
+        <label className='font-bold '>
+          Vị trí<sup className='text-red-500'>*</sup>
+        </label>
         <input
-          className={` block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
+          className={`${errors.position ? 'ring-red-600' : ''} block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
           {...register('position')}
           placeholder='Nhập vị trí nhân viên'
         />
+        <div className={`text-red-500 absolute text-[12px] ${errors.position ? 'visible' : 'invisible'}`}>
+          {errors.position?.message}
+        </div>
       </div>
       <div className='w-[100%] sm:w-[48%] md:w-[29%] mt-5 relative'>
         <label className='font-bold '>
@@ -99,11 +113,13 @@ const EditEmployee = ({ data, closeModal, refetch }: IProp) => {
       </div>
 
       <div className='w-[100%] sm:w-[48%] md:w-[29%] mt-5 relative'>
-        <label className='font-bold '>CCCD/CMT</label>
+        <label className='font-bold '>
+          CCCD/CMT<sup className='text-red-500'>*</sup>
+        </label>
         <input
           className={`${errors.identificationNumber ? 'ring-red-600' : ''} block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
           {...register('identificationNumber')}
-          placeholder='Nhập CCCD/CMT nhân viên'
+          placeholder='CCCD/CMT'
         />
         <div className={`text-red-500 absolute text-[12px] ${errors.identificationNumber ? 'visible' : 'invisible'}`}>
           {errors.identificationNumber?.message}
@@ -118,31 +134,49 @@ const EditEmployee = ({ data, closeModal, refetch }: IProp) => {
           {...register('phone', {
             required: 'Số điện thoại không được bỏ trống'
           })}
-          placeholder='Nhập số điện thoại nhân viên'
+          placeholder='Số điện thoại'
         />
         <div className={`text-red-500 absolute text-[12px] ${errors.phone ? 'visible' : 'invisible'}`}>
           {errors.phone?.message}
         </div>
       </div>
       <div className='w-[100%] sm:w-[48%] md:w-[29%] mt-5 relative'>
-        <label className='font-bold '>Địa chỉ</label>
+        <label className='font-bold '>
+          Địa chỉ<sup className='text-red-500'>*</sup>
+        </label>
         <input
-          className={` block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
+          className={`${errors.address ? 'ring-red-600' : ''} block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
           {...register('address')}
-          placeholder='Nhập địa chỉ nhân viên'
+          placeholder='Địa chỉ'
         />
+        <div className={`text-red-500 absolute text-[12px] ${errors.address ? 'visible' : 'invisible'}`}>
+          {errors.address?.message}
+        </div>
       </div>
       <div className='w-[100%] sm:w-[48%] md:w-[29%] mt-5 relative'>
-        <label className='font-bold '>Ngày sinh</label>
+        <label className='font-bold '>
+          Ngày sinh<sup className='text-red-500'>*</sup>
+        </label>
         <input
           type='date'
-          className={` block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-          {...register('dob')}
+          className={`${errors.dob ? 'ring-red-600' : ''} block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
+          {...register('dob', {
+            required: 'Ngày sinh không được bỏ trống',
+            max: {
+              value: currentDate(),
+              message: 'Ngày sinh không hợp lệ'
+            }
+          })}
         />
+        <div className={`text-red-500 absolute text-[12px] ${errors.dob ? 'visible' : 'invisible'}`}>
+          {errors.dob?.message}
+        </div>
       </div>
 
       <div className='w-[100%] sm:w-[48%] md:w-[29%] mt-5 relative'>
-        <label className='font-bold '>Giới tính</label>
+        <label className='font-bold '>
+          Giới tính<sup className='text-red-500'>*</sup>
+        </label>
         <select
           className={` block w-full rounded-md border-0 py-1.5 px-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
           {...register('gender')}
@@ -158,20 +192,10 @@ const EditEmployee = ({ data, closeModal, refetch }: IProp) => {
         <div className='flex flex-wrap w-[70%] justify-between'>
           {permissionsList?.map((e) => (
             <div className='flex w-[100%] md:w-[48%] gap-4 items-center' key={e.id}>
-              <input
-                type='radio'
-                className='rounded-lg'
-                {...register('permissions', {
-                  required: true
-                })}
-                value={e.value}
-              />
+              <input type='radio' className='rounded-lg' {...register('permissions')} value={e.value} />
               <label>{e.title}</label>
             </div>
           ))}
-        </div>
-        <div className={`text-red-500 absolute text-[12px] ${errors.permissions ? 'visible' : 'invisible'}`}>
-          Hãy chọn quyền cho nhân viên
         </div>
       </div>
       <div className='w-full flex justify-end'>
@@ -180,7 +204,7 @@ const EditEmployee = ({ data, closeModal, refetch }: IProp) => {
           className='middle my-3 none center mr-4 rounded-lg bg-[#0070f4] py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-[#0072f491] focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
           data-ripple-light='true'
         >
-          Sửa
+          {editEmployee.isLoading ? <LoadingIcon /> : 'Sửa'}
         </button>
       </div>
     </form>
