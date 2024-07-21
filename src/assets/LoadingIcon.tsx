ae64bd9150e292de3c8e8cd45fd75c584ff3b4a9
ThangNH <nhoctrumgenz2002@gmail.com>
2024-07-21 12:27:48 +0700

    fix: add className loading

--- src/assets/LoadingIcon.tsx
+++ src/assets/LoadingIcon.tsx
@@ -1,9 +1,9 @@
-const LoadingIcon = () => {
+const LoadingIcon = ({ className = 'w-4 h-4' }) => {
   return (
     <div role='status'>
       <svg
         aria-hidden='true'
-        className='w-4 h-4 text-gray-300 animate-spin dark:text-gray-600 fill-blue-600'
+        className={`${className} text-gray-300 animate-spin dark:text-gray-600 fill-blue-600`}
         viewBox='0 0 100 101'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
