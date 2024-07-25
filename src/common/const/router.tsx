4640cbec7947c4de8f5b815f127c833edbfc501b
ThangNH <nhoctrumgenz2002@gmail.com>
2024-07-25 19:41:44 +0700

    fix: delete suspens

--- src/common/const/router.tsx
+++ src/common/const/router.tsx
@@ -1,4 +1,4 @@
-import { EyeIcon, UserGroupIcon, ClipboardDocumentListIcon, DocumentTextIcon } from '@heroicons/react/24/outline'
+import { EyeIcon, UserGroupIcon, ClipboardDocumentListIcon, DocumentTextIcon,CircleStackIcon } from '@heroicons/react/24/outline'
 //Admin: tổng quan, employee, hợp đồng, hợp đồng cũ, hợp đồng mẫu, gửi mail, ký, hợp đòng với admin tdocman
 //AdminOficer: tổng quan, employee, hợp đồng, hợp đồng cũ, hợp đồng mẫu, gửi mail,
 //sale: tổng quan, hợp đồng, hợp đồng cũ, hợp đồng mẫu, gửi mail,
@@ -39,6 +39,12 @@ export const routerAdmin = [
     title: 'Loại hợp đồng',
     slug: '/type-contract',
     icon: <ClipboardDocumentListIcon className='h-4 w-4' />
+  },
+  {
+    id: 7,
+    title: 'Bảng lương',
+    slug: '/salary',
+    icon: <CircleStackIcon className='h-4 w-4' />
   }
 ]
 export const routerAdminOfficer = [
@@ -77,6 +83,12 @@ export const routerAdminOfficer = [
     title: 'Loại hợp đồng',
     slug: '/type-contract',
     icon: <ClipboardDocumentListIcon className='h-4 w-4' />
+  },
+  {
+    id: 7,
+    title: 'Bảng lương',
+    slug: '/salary',
+    icon: <CircleStackIcon className='h-4 w-4' />
   }
 ]
 export const routerSale = [
@@ -103,5 +115,11 @@ export const routerSale = [
     title: 'Loại hợp đồng',
     slug: '/type-contract',
     icon: <ClipboardDocumentListIcon className='h-4 w-4' />
+  },
+  {
+    id: 7,
+    title: 'Bảng lương',
+    slug: '/salary',
+    icon: <CircleStackIcon className='h-4 w-4' />
   }
 ]
