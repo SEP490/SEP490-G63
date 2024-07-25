7d43ba7e1ac47b8c8ff3fa6736f1127f1688a648
ThangNH <nhoctrumgenz2002@gmail.com>
2024-07-25 18:37:59 +0700

    fix: change screen type contract

--- src/pages/Admin/TypeContract.tsx
+++ src/pages/Admin/TypeContract.tsx
@@ -103,10 +103,10 @@ const TypeContract = () => {
 
   return (
     <div className='bg-[#e8eaed] h-full overflow-auto'>
-      <div className='flex flex-wrap py-4'>
-        <div className=' w-full px-5'>
-          <div className='flex gap-3 justify-between w-full'>
-            <div className='flex w-[50%]'>
+      <div className='flex flex-wrap'>
+        <div className='w-full px-3'>
+          <div className='flex gap-3 justify-between w-full py-3 h-[60px]'>
+            <div className='flex md:w-[50%] w-[70%]'>
               <div className='relative'>
                 <div className='absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none'>
                   <svg
@@ -127,116 +127,123 @@ const TypeContract = () => {
               <input
                 type='text'
                 id='table-search'
-                className='block p-2 ps-10 w-full text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                placeholder='Tìm kiếm mẫu hợp đồng'
+                className='shadow-md block p-2 ps-10 w-full text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                placeholder='Tìm kiếm'
                 // onChange={handChangeInputSearch}
               />
             </div>
-
             <button
               type='button'
               onClick={() => setAddModal(true)}
-              className='rounded-md flex gap-1 bg-main-color px-4 py-2 text-sm font-medium text-white hover:bg-hover-main focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75'
+              className='rounded-md flex gap-1 bg-main-color px-4 py-2 text-xs sm:text-sm items-center font-medium text-white hover:bg-hover-main focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75'
             >
               <PlusIcon className='h-5 w-5' /> Tạo mới
             </button>
           </div>
-          <div className='shadow-md sm:rounded-lg my-3  max-h-[75vh]'>
-            <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 '>
-              <thead className='text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 shadow-md'>
-                <tr>
-                  <th className='px-3 py-3'>STT</th>
-                  <th className='px-3 py-3'>Loại hợp đồng</th>
-                  <th className='px-3 py-3'>Mô tả</th>
-                  <th className='px-3 py-3 w-1'></th>
-                </tr>
-              </thead>
 
-              <tbody className='w-full '>
-                {!isLoading && !isFetching ? (
-                  dataTable?.map((d: any, index: number) => (
-                    <tr
-                      key={d.id}
-                      className='w-full bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 '
-                    >
-                      <td className='px-3 py-4'>{page * size + index + 1}</td>
-                      <td className='px-3 py-4'>{d.title}</td>
-                      <td className='px-3 py-4'>{d.description}</td>
+          <div className='overflow-auto'>
+            <div className='shadow-md sm:rounded-lg h-fit'>
+              <table className='w-full text-sm text-left rtl:text-right text-black dark:text-gray-400 '>
+                <thead className='text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 '>
+                  <tr>
+                    <th className='px-3 py-3 w-[30px]' align='center'>
+                      STT
+                    </th>
+                    <th className='px-3 py-3 w-[200px]'>Loại hợp đồng</th>
+                    <th className='px-3 py-3'>Mô tả</th>
+                    <th className='px-3 py-3 w-1'></th>
+                  </tr>
+                </thead>
 
-                      <td className='px-3 py-4 w-[20px] cursor-pointer ho'>
-                        <Menu as='div' className='relative inline-block text-left '>
-                          <Menu.Button className='flex justify-center items-center gap-3 cursor-pointer hover:text-blue-500'>
-                            <EllipsisVerticalIcon className='h-7 w-7' title='Hành động' />
-                          </Menu.Button>
+                <tbody className='w-full '>
+                  {!isLoading &&
+                    !isFetching &&
+                    dataTable?.map((d: any, index: number) => (
+                      <tr
+                        key={d.id}
+                        className='w-full bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 '
+                      >
+                        <td className='px-3 py-4 w-[30px]' align='center'>
+                          {page * size + index + 1 < 10 ? `0${page * size + index + 1}` : page * size + index + 1}
+                        </td>
+                        <td className='px-3 py-4'>
+                        <div className='w-[150px] truncate ...'>{d.title}</div></td>
+                        <td className='px-3 py-4'>
+                          <div className='w-[800px] truncate ...'>{d.description}</div>
+                        </td>
 
-                          <Transition
-                            as={Fragment}
-                            enter='transition ease-out duration-100'
-                            enterFrom='transform opacity-0 scale-95'
-                            enterTo='transform opacity-100 scale-100'
-                            leave='transition ease-in duration-75'
-                            leaveFrom='transform opacity-100 scale-100'
-                            leaveTo='transform opacity-0 scale-95'
-                          >
-                            <Menu.Items className='absolute right-8 top-[-100%] z-50 mt-2 w-24 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none'>
-                              <Menu.Item>
-                                {({ active }) => (
-                                  <button
-                                    title='Sửa'
-                                    onClick={() => {
-                                      setOpenModal(true)
-                                      setSelectedContract(d)
-                                    }}
-                                    className={`${
-                                      active ? 'bg-blue-500 text-white' : 'text-gray-900'
-                                    } group flex w-full items-center  gap-3 rounded-md px-2 py-2 text-sm `}
-                                  >
-                                    <Cog6ToothIcon className='h-5' /> Sửa
-                                  </button>
-                                )}
-                              </Menu.Item>
-                              <Menu.Item>
-                                {({ active }) => (
-                                  <button
-                                    title='Xóa'
-                                    onClick={() => {
-                                      setDeleteModal(true)
-                                      setSelectedContract(d)
-                                    }}
-                                    className={`${
-                                      active ? 'bg-blue-500 text-white' : 'text-gray-900'
-                                    } group flex w-full items-center gap-3 rounded-md px-2 py-2 text-sm `}
-                                  >
-                                    <NoSymbolIcon className='h-5' /> Xóa
-                                  </button>
-                                )}
-                              </Menu.Item>
-                            </Menu.Items>
-                          </Transition>
-                        </Menu>
-                      </td>
-                    </tr>
-                  ))
-                ) : (
-                  <></>
-                )}
-              </tbody>
-            </table>
-            {(isLoading || isFetching) && (
-              <Loading loading={isLoading || isFetching}>
-                <div className='w-full min-h-[200px] opacity-75 bg-gray-50 flex items-center justify-center'></div>
-              </Loading>
-            )}
-            {!isLoading && !isFetching && (data == null || data?.content?.length == 0) && (
-              <div className='w-full min-h-[200px] opacity-75 bg-gray-50 flex items-center justify-center'>
-                <div className='flex flex-col justify-center items-center opacity-60'>
-                  <DocumentIcon />
-                  Chưa có loại hợp đồng
+                        <td className='px-3 py-4 w-[20px] cursor-pointer ho'>
+                          <Menu as='div' className='relative inline-block text-left '>
+                            <Menu.Button className='flex justify-center items-center gap-3 cursor-pointer hover:text-blue-500'>
+                              <EllipsisVerticalIcon className='h-7 w-7' title='Hành động' />
+                            </Menu.Button>
+
+                            <Transition
+                              as={Fragment}
+                              enter='transition ease-out duration-100'
+                              enterFrom='transform opacity-0 scale-95'
+                              enterTo='transform opacity-100 scale-100'
+                              leave='transition ease-in duration-75'
+                              leaveFrom='transform opacity-100 scale-100'
+                              leaveTo='transform opacity-0 scale-95'
+                            >
+                              <Menu.Items className='absolute right-8 top-[-100%] z-50 mt-2 w-24 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none'>
+                                <Menu.Item>
+                                  {({ active }) => (
+                                    <button
+                                      title='Sửa'
+                                      onClick={() => {
+                                        setOpenModal(true)
+                                        setSelectedContract(d)
+                                      }}
+                                      className={`${
+                                        active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                                      } group flex w-full items-center  gap-3 rounded-md px-2 py-2 text-sm `}
+                                    >
+                                      <Cog6ToothIcon className='h-5' /> Sửa
+                                    </button>
+                                  )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                  {({ active }) => (
+                                    <button
+                                      title='Xóa'
+                                      onClick={() => {
+                                        setDeleteModal(true)
+                                        setSelectedContract(d)
+                                      }}
+                                      className={`${
+                                        active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                                      } group flex w-full items-center gap-3 rounded-md px-2 py-2 text-sm `}
+                                    >
+                                      <NoSymbolIcon className='h-5' /> Xóa
+                                    </button>
+                                  )}
+                                </Menu.Item>
+                              </Menu.Items>
+                            </Transition>
+                          </Menu>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+              {(isLoading || isFetching) && (
+                <Loading loading={isLoading || isFetching}>
+                  <div className='w-full min-h-[60vh] opacity-75 bg-gray-50 flex items-center justify-center'></div>
+                </Loading>
+              )}
+              {!isLoading && !isFetching && (data == null || data?.content?.length == 0) && (
+                <div className='w-full min-h-[60vh] opacity-75 bg-gray-50 flex items-center justify-center'>
+                  <div className='flex flex-col justify-center items-center opacity-60'>
+                    <DocumentIcon />
+                    Chưa có hợp đồng mẫu
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
-          {!isLoading && !isFetching && dataTable && dataTable?.length != 0 && (
+          {!isLoading && !isFetching && data && data?.content?.length != 0 && (
             <Pagination
               totalPages={totalPage}
               currentPage={page + 1}
@@ -372,6 +379,89 @@ const TypeContract = () => {
         </Dialog>
       </Transition>
     </div>
+    // <div className='bg-[#e8eaed] h-full overflow-auto'>
+    //   <div className='flex flex-wrap py-4'>
+    //     <div className=' w-full px-5'>
+    //       <div className='flex gap-3 justify-between w-full'>
+    //         <div className='flex w-[50%]'>
+    //           <div className='relative'>
+    //             <div className='absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none'>
+    //               <svg
+    //                 className='w-5 h-5 text-gray-500 dark:text-gray-400'
+    //                 aria-hidden='true'
+    //                 fill='currentColor'
+    //                 viewBox='0 0 20 20'
+    //                 xmlns='http://www.w3.org/2000/svg'
+    //               >
+    //                 <path
+    //                   fillRule='evenodd'
+    //                   d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
+    //                   clipRule='evenodd'
+    //                 ></path>
+    //               </svg>
+    //             </div>
+    //           </div>
+    //           <input
+    //             type='text'
+    //             id='table-search'
+    //             className='block p-2 ps-10 w-full text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+    //             placeholder='Tìm kiếm mẫu hợp đồng'
+    //             // onChange={handChangeInputSearch}
+    //           />
+    //         </div>
+
+    //         <button
+    //           type='button'
+    //           onClick={() => setAddModal(true)}
+    //           className='rounded-md flex gap-1 bg-main-color px-4 py-2 text-sm font-medium text-white hover:bg-hover-main focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75'
+    //         >
+    //           <PlusIcon className='h-5 w-5' /> Tạo mới
+    //         </button>
+    //       </div>
+    //       <div className='shadow-md sm:rounded-lg my-3  max-h-[75vh]'>
+    //         <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 '>
+    //           <thead className='text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 shadow-md'>
+
+    //           </thead>
+
+    //           <tbody className='w-full '>
+    //             {!isLoading && !isFetching ? (
+    //               dataTable?.map((d: any, index: number) => (
+
+    //               ))
+    //             ) : (
+    //               <></>
+    //             )}
+    //           </tbody>
+    //         </table>
+    //         {(isLoading || isFetching) && (
+    //           <Loading loading={isLoading || isFetching}>
+    //             <div className='w-full min-h-[200px] opacity-75 bg-gray-50 flex items-center justify-center'></div>
+    //           </Loading>
+    //         )}
+    //         {!isLoading && !isFetching && (data == null || data?.content?.length == 0) && (
+    //           <div className='w-full min-h-[200px] opacity-75 bg-gray-50 flex items-center justify-center'>
+    //             <div className='flex flex-col justify-center items-center opacity-60'>
+    //               <DocumentIcon />
+    //               Chưa có loại hợp đồng
+    //             </div>
+    //           </div>
+    //         )}
+    //       </div>
+    //       {!isLoading && !isFetching && dataTable && dataTable?.length != 0 && (
+    //         <Pagination
+    //           totalPages={totalPage}
+    //           currentPage={page + 1}
+    //           size={size}
+    //           setSize={setSize}
+    //           setPage={setPage}
+    //           onPageChange={handlePageChange}
+    //         />
+    //       )}
+    //     </div>
+    //   </div>
+
+    // </div>
   )
 }
 export default TypeContract
