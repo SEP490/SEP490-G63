f2cb90ed0a9aebf6d5c5ef01a7bdd07419bc2dfb
ThangNH <nhoctrumgenz2002@gmail.com>
2024-07-25 19:22:12 +0700

    feat: search type contract

--- src/pages/Admin/TypeContract.tsx
+++ src/pages/Admin/TypeContract.tsx
@@ -1,10 +1,12 @@
 import { Dialog, Menu, Transition } from '@headlessui/react'
 import { Cog6ToothIcon, EllipsisVerticalIcon, NoSymbolIcon, PlusIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { AxiosError } from 'axios'
+import { debounce } from 'lodash'
 import moment from 'moment'
 import { Fragment, useEffect, useRef, useState } from 'react'
 import { useMutation, useQuery } from 'react-query'
 import { useNavigate } from 'react-router-dom'
+import LoadingIcon from '~/assets/LoadingIcon'
 import DocumentIcon from '~/assets/svg/document'
 import EditTemplateContract from '~/components/Admin/TemplateContract/EditTemplateContract'
 import ViewTemplateContract from '~/components/Admin/TemplateContract/ViewTemplateContract'
@@ -27,14 +29,14 @@ const TypeContract = () => {
   const [deleteModal, setDeleteModal] = useState(false)
   const [openModal, setOpenModal] = useState(false)
   const [addModal, setAddModal] = useState(false)
-  const navigate = useNavigate()
+  const [searchData, setSearchData] = useState('')
   const [selectedContract, setSelectedContract] = useState<any>(null)
   const { successNotification, errorNotification } = useToast()
   const [dataTable, setDataTable] = useState([])
   const prevPageRef = useRef(page)
   const prevSizeRef = useRef(size)
-  const { data, error, isError, isLoading, refetch, isFetching } = useQuery('type-contract', () =>
-    getContractType({ page: page, size: size })
+  const { data, error, isError, isLoading, refetch, isFetching } = useQuery(['type-contract', searchData], () =>
+    getContractType({ page: page, size: size, title: searchData })
   )
   const handlePageChange = (page: any) => {
     setPage(page - 1)
@@ -58,31 +60,34 @@ const TypeContract = () => {
   const handleDelete = () => {
     if (selectedContract) deleteType.mutate(selectedContract.id)
   }
-  const handleEditType = async (data: any) => {
-    try {
-      const result = await updateTypeContract({ id: selectedContract.id, data: data })
-      if (result?.data) {
-        successNotification('Chỉnh sửa thông tin thành công')
-        handleCloseModal()
-        refetch()
-      }
-    } catch (e) {
-      console.log(e)
-      errorNotification('Chỉnh sửa thất bại thất bại')
+  const editQuery = useMutation(updateTypeContract, {
+    onError: (error: AxiosError<{ message: string }>) => {
+      errorNotification(error.response?.data.message || 'Lỗi hệ thống')
+    },
+    onSuccess: () => {
+      successNotification('Chỉnh sửa thông tin thành công')
+      handleCloseModal()
+      refetch()
     }
+  })
+  const createQuery = useMutation(createTypeContract, {
+    onError: (error: AxiosError<{ message: string }>) => {
+      errorNotification(error.response?.data.message || 'Lỗi hệ thống')
+    },
+    onSuccess: () => {
+      successNotification('Tạo mới thành công')
+      handleCloseModal()
+      refetch()
+    }
+  })
+  const handleEditType = async (data: any) => {
+    editQuery.mutate({ id: selectedContract.id, data: data })
   }
   const handleAddTypeContract = async (data: any) => {
-    try {
-      const result = await createTypeContract(data)
-      if (result?.data) {
-        successNotification('Tạo mới thành công')
-        handleCloseModal()
-        refetch()
-      }
-    } catch (e) {
-      console.log(e)
-      errorNotification('Tạo mới thất bại')
-    }
+    createQuery.mutate(data)
+  }
+  const handChangeInputSearch = (e: any) => {
+    setSearchData(e.target.value)
   }
   useEffect(() => {
     if (isError) {
@@ -129,7 +134,7 @@ const TypeContract = () => {
                 id='table-search'
                 className='shadow-md block p-2 ps-10 w-full text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                 placeholder='Tìm kiếm'
-                // onChange={handChangeInputSearch}
+                onChange={debounce(handChangeInputSearch, 300)}
               />
             </div>
             <button
@@ -199,7 +204,7 @@ const TypeContract = () => {
                                       }}
                                       className={`${
                                         active ? 'bg-blue-500 text-white' : 'text-gray-900'
-                                      } group flex w-full items-center  gap-3 rounded-md px-2 py-2 text-sm `}
+                                      } group flex w-full items-center  gap-3 rounded-md px-2 py-1 text-sm `}
                                     >
                                       <Cog6ToothIcon className='h-5' /> Sửa
                                     </button>
@@ -215,7 +220,7 @@ const TypeContract = () => {
                                       }}
                                       className={`${
                                         active ? 'bg-blue-500 text-white' : 'text-gray-900'
-                                      } group flex w-full items-center gap-3 rounded-md px-2 py-2 text-sm `}
+                                      } group flex w-full items-center gap-3 rounded-md px-2 py-1 text-sm `}
                                     >
                                       <NoSymbolIcon className='h-5' /> Xóa
                                     </button>
@@ -294,7 +299,7 @@ const TypeContract = () => {
                         data-ripple-light='true'
                         onClick={() => handleDelete()}
                       >
-                        Đồng ý
+                        {deleteType.isLoading ? <LoadingIcon /> : 'Đồng ý'}
                       </button>
                     </div>
                   </div>
@@ -334,7 +339,11 @@ const TypeContract = () => {
                     <div className='font-semibold'>Chỉnh sửa</div>
                     <XMarkIcon className='h-5 w-5 mr-3 mb-3 cursor-pointer' onClick={handleCloseModal} />
                   </div>
-                  <EditTypeContract selectedContract={selectedContract} onSubmit={handleEditType} />
+                  <EditTypeContract
+                    selectedContract={selectedContract}
+                    onSubmit={handleEditType}
+                    loading={editQuery.isLoading}
+                  />
                 </Dialog.Panel>
               </Transition.Child>
             </div>
@@ -372,7 +381,7 @@ const TypeContract = () => {
                     <div className='font-semibold'>Tạo mới</div>
                     <XMarkIcon className='h-5 w-5 mr-3 mb-3 cursor-pointer' onClick={handleCloseModal} />
                   </div>
-                  <AddNewTypeContract onSubmit={handleAddTypeContract} />
+                  <AddNewTypeContract onSubmit={handleAddTypeContract} loading={createQuery.isLoading} />
                 </Dialog.Panel>
               </Transition.Child>
             </div>
@@ -380,89 +389,6 @@ const TypeContract = () => {
         </Dialog>
       </Transition>
     </div>
-    // <div className='bg-[#e8eaed] h-full overflow-auto'>
-    //   <div className='flex flex-wrap py-4'>
-    //     <div className=' w-full px-5'>
-    //       <div className='flex gap-3 justify-between w-full'>
-    //         <div className='flex w-[50%]'>
-    //           <div className='relative'>
-    //             <div className='absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none'>
-    //               <svg
-    //                 className='w-5 h-5 text-gray-500 dark:text-gray-400'
-    //                 aria-hidden='true'
-    //                 fill='currentColor'
-    //                 viewBox='0 0 20 20'
-    //                 xmlns='http://www.w3.org/2000/svg'
-    //               >
-    //                 <path
-    //                   fillRule='evenodd'
-    //                   d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
-    //                   clipRule='evenodd'
-    //                 ></path>
-    //               </svg>
-    //             </div>
-    //           </div>
-    //           <input
-    //             type='text'
-    //             id='table-search'
-    //             className='block p-2 ps-10 w-full text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-    //             placeholder='Tìm kiếm mẫu hợp đồng'
-    //             // onChange={handChangeInputSearch}
-    //           />
-    //         </div>
-
-    //         <button
-    //           type='button'
-    //           onClick={() => setAddModal(true)}
-    //           className='rounded-md flex gap-1 bg-main-color px-4 py-2 text-sm font-medium text-white hover:bg-hover-main focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75'
-    //         >
-    //           <PlusIcon className='h-5 w-5' /> Tạo mới
-    //         </button>
-    //       </div>
-    //       <div className='shadow-md sm:rounded-lg my-3  max-h-[75vh]'>
-    //         <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 '>
-    //           <thead className='text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400 shadow-md'>
-
-    //           </thead>
-
-    //           <tbody className='w-full '>
-    //             {!isLoading && !isFetching ? (
-    //               dataTable?.map((d: any, index: number) => (
-
-    //               ))
-    //             ) : (
-    //               <></>
-    //             )}
-    //           </tbody>
-    //         </table>
-    //         {(isLoading || isFetching) && (
-    //           <Loading loading={isLoading || isFetching}>
-    //             <div className='w-full min-h-[200px] opacity-75 bg-gray-50 flex items-center justify-center'></div>
-    //           </Loading>
-    //         )}
-    //         {!isLoading && !isFetching && (data == null || data?.content?.length == 0) && (
-    //           <div className='w-full min-h-[200px] opacity-75 bg-gray-50 flex items-center justify-center'>
-    //             <div className='flex flex-col justify-center items-center opacity-60'>
-    //               <DocumentIcon />
-    //               Chưa có loại hợp đồng
-    //             </div>
-    //           </div>
-    //         )}
-    //       </div>
-    //       {!isLoading && !isFetching && dataTable && dataTable?.length != 0 && (
-    //         <Pagination
-    //           totalPages={totalPage}
-    //           currentPage={page + 1}
-    //           size={size}
-    //           setSize={setSize}
-    //           setPage={setPage}
-    //           onPageChange={handlePageChange}
-    //         />
-    //       )}
-    //     </div>
-    //   </div>
-
-    // </div>
   )
 }
 export default TypeContract
--- src/services/type-contract.service.ts
+++ src/services/type-contract.service.ts
@@ -2,9 +2,10 @@ import axiosInstance from '~/config/axiosConfig'
 type RequestQuery = {
   page: number
   size: number
+  title: string
 }
-export const getContractType = async ({ page, size }: RequestQuery) => {
-  const result = await axiosInstance.get(`contract-type?page=${page}&size=${size}`)
+export const getContractType = async ({ page, size, title }: RequestQuery) => {
+  const result = await axiosInstance.get(`contract-type?page=${page}&size=${size}&title=${title}`)
   return result?.data
 }
 export const getContractTypeById = async (id: string) => {
