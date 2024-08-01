19e01e9df3c1036b958e3f1852e69b4a3d134762
tudda <tentufancr7@gmail.com>
2024-08-01 23:14:13 +0700

    config

--- src/common/const/index.ts
+++ src/common/const/index.ts
@@ -1,7 +1,11 @@
+export const BASE_URL_FE = 'http://tdocman.id.vn/'
+export const BASE_URL = 'http://api.tdocman.id.vn/api/'
+export const BASE_ADMIN_URL = 'http://admin.api.tdocman.id.vn/api/'
+
+// export const BASE_URL_FE = 'http://localhost:3000/'
 // export const BASE_URL = 'http://localhost:8080/'
-// // export const BASE_URL = 'http://api.tdocman.id.vn/api/'
-export const BASE_URL_FE = 'http://localhost:3000/'
 // export const BASE_ADMIN_URL = 'http://localhost:8081/'
+
 export const BASE_PROVINCE_URL = 'https://vapi.vnappmob.com'
 export const INTERNAL_SERVER_ERROR = 500
 export const NOT_FOUND = 404
@@ -11,6 +15,3 @@ export const FORBIDDEN = 403
 export const GATEWAY_TIME_OUT = 504
 export const REQUEST_SUCCESS = 200
 export const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
-export const BASE_URL = 'http://api.tdocman.id.vn/api/'
-export const BASE_ADMIN_URL = 'http://admin.api.tdocman.id.vn/api/'
