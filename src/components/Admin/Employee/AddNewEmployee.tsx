be32f426bbf2a9b9322aab000866bf28cc53f56d
ThangNH <nhoctrumgenz2002@gmail.com>
2024-08-02 03:27:34 +0700

    fix:employee

--- src/components/Admin/Employee/AddNewEmployee.tsx
+++ src/components/Admin/Employee/AddNewEmployee.tsx
@@ -32,8 +32,11 @@ const AddNewEmployee = ({ closeModal, refetch }: IProp) => {
   const {
     register,
     handleSubmit,
+    watch,
+    setValue,
+    getValues,
     formState: { errors }
-  } = useForm<FromType>()
+  } = useForm<FromType>({ defaultValues: { permissions: 'SALE' } })
   const { successNotification, errorNotification } = useToast()
   const addNewEmployeeQuery = useMutation(createEmployee, {
     onError: (error: AxiosError<{ message: string }>) => {
@@ -49,6 +52,8 @@ const AddNewEmployee = ({ closeModal, refetch }: IProp) => {
       }
     }
   })
+  console.log(watch('permissions'))
+
   const onSubmit: SubmitHandler<FromType> = async (data) => {
     addNewEmployeeQuery.mutate({
       ...data,
@@ -276,10 +281,13 @@ const AddNewEmployee = ({ closeModal, refetch }: IProp) => {
             <div className='relative flex w-[100%] md:w-[48%] gap-4 items-center' key={e.id}>
               <input
                 type='radio'
-                {...register('permissions')}
+                name='permissions'
                 className='rounded-lg'
                 value={e.value}
-                defaultChecked={e.value == 'SALE'}
+                defaultChecked={e.value == getValues('permissions')}
+                onChange={() => {
+                  setValue('permissions', e.value)
+                }}
               />
               <label className='flex items-center gap-1'>
                 {e.title}
@@ -287,6 +295,9 @@ const AddNewEmployee = ({ closeModal, refetch }: IProp) => {
               </label>
             </div>
           ))}
+        </div>
+        <div className={`text-red-500 absolute text-[12px] ${errors.permissions ? 'visible' : 'invisible'}`}>
+          {errors.permissions?.message}
         </div>
       </div>
       <div className='w-full flex justify-end'>
