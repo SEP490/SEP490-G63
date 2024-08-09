ba4cd6cdedf21980745d653440ea141d7f1f0ea6
ThangNH <nhoctrumgenz2002@gmail.com>
2024-08-09 11:09:16 +0700

    Fix: show slip

--- src/components/Admin/Salary/PaySlipFormula.tsx
+++ src/components/Admin/Salary/PaySlipFormula.tsx
@@ -1,11 +1,13 @@
+import { useQuery } from 'react-query'
 import LoadingIcon from '~/assets/LoadingIcon'
 import { formatPrice } from '~/common/utils/formatPrice'
 import { useFormData } from '~/context/formProvider'
+import { getPaySlipFormula } from '~/services/pay.formula.service'
 const PaySlipFormula = () => {
-  const { listData, loading } = useFormData()
+  const { data, isLoading } = useQuery(['pay-slip'], () => getPaySlipFormula())
   return (
     <>
-      {loading ? (
+      {isLoading ? (
         <div className='w-full h-[300px] flex justify-center items-center'>
           <LoadingIcon className='w-10 h-10' />
         </div>
@@ -14,15 +16,17 @@ const PaySlipFormula = () => {
           <table className='w-full text-sm text-left rtl:text-right text-black dark:text-gray-400 '>
             <tr className='bg-white dark:bg-gray-800'>
               <td className='px-1 py-2 font-bold'>Giá trị hợp đồng(VND)</td>
-              {listData?.map((d: any) => (
+              {data?.object?.content?.map((d: any) => (
                 <td className='px-1 py-2 border-l' key={d.id} align='center'>
-                  {formatPrice(d.toValueContract)}
+                  {formatPrice(d.fromValueContract) == 0 && formatPrice(d.toValueContract) != 0
+                    ? `< ${formatPrice(d.toValueContract)}`
+                    : `${formatPrice(d.fromValueContract)} - ${formatPrice(d.toValueContract)}`}
                 </td>
               ))}
             </tr>
             <tr className='bg-white dark:bg-gray-800'>
               <td className='px-1 py-2 font-bold'>Lương cứng(VND)</td>
-              {listData?.map((d: any) => (
+              {data?.object?.content?.map((d: any) => (
                 <td className='px-1 py-2 border-l' key={d.id} align='center'>
                   {formatPrice(d.baseSalary)}
                 </td>
@@ -30,7 +34,7 @@ const PaySlipFormula = () => {
             </tr>
             <tr className='bg-white dark:bg-gray-800'>
               <td className='px-1 py-2 font-bold'>% doanh số</td>
-              {listData?.map((d: any) => (
+              {data?.object?.content?.map((d: any) => (
                 <td className='px-1 py-2 border-l' key={d.id} align='center'>
                   {d.commissionPercentage}(%)
                 </td>
@@ -38,7 +42,7 @@ const PaySlipFormula = () => {
             </tr>
             <tr className='bg-white dark:bg-gray-800'>
               <td className='px-1 py-2 font-bold'>% triển khai KH</td>
-              {listData?.map((d: any) => (
+              {data?.object?.content?.map((d: any) => (
                 <td className='px-1 py-2 border-l' key={d.id} align='center'>
                   {d.clientDeploymentPercentage}(%)
                 </td>
@@ -46,7 +50,7 @@ const PaySlipFormula = () => {
             </tr>
             <tr className='bg-white dark:bg-gray-800'>
               <td className='px-1 py-2 font-bold'>Thưởng đạt ngưỡng(VND)</td>
-              {listData?.map((d: any) => (
+              {data?.object?.content?.map((d: any) => (
                 <td className='px-1 py-2 border-l' key={d.id} align='center'>
                   {formatPrice(d.bonusReachesThreshold)}
                 </td>
@@ -54,7 +58,7 @@ const PaySlipFormula = () => {
             </tr>
             <tr className='bg-white dark:bg-gray-800'>
               <td className='px-1 py-2 font-bold'>Trợ cấp ăn(VND)</td>
-              {listData?.map((d: any) => (
+              {data?.object?.content?.map((d: any) => (
                 <td className='px-1 py-2 border-l' key={d.id} align='center'>
                   {formatPrice(d.foodAllowance)}
                 </td>
@@ -62,7 +66,7 @@ const PaySlipFormula = () => {
             </tr>
             <tr className='bg-white dark:bg-gray-800'>
               <td className='px-1 py-2 font-bold'>Phụ cấp xăng xe,điện thoại(VND)</td>
-              {listData?.map((d: any) => (
+              {data?.object?.content?.map((d: any) => (
                 <td className='px-1 py-2 border-l' key={d.id} align='center'>
                   {formatPrice(d.transportationOrPhoneAllowance)}
                 </td>
